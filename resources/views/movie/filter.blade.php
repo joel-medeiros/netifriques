600e4ef5768477891af8fa72a834a89f7d9ae339
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-05-07 20:22:37 -0300

    correting labels

--- resources/views/movie/filter.blade.php
+++ resources/views/movie/filter.blade.php
@@ -7,7 +7,7 @@
         <!-- Search input-->
         <div class="form-group">
             <label class=" control-label" for="title">Title</label>
-            <input id="title" name="title" type="search" placeholder="E.g It Could Happen to You" class="form-control" value="{{old('title')}}">
+            <input id="title" name="title" type="search" placeholder="i.g It Could Happen to You" class="form-control" value="{{old('title')}}">
             <p class="help-block">Find by the title</p>
         </div>
 
@@ -19,17 +19,17 @@
             </div>
             <div class="form-group">
                 <label class=" control-label" for="actor">Actor</label>
-                <input id="actor" name="actor" type="search" placeholder="E.g Nicolas Cage" class="form-control" value="{{old('actor')}}">
+                <input id="actor" name="actor" type="search" placeholder="i.g Nicolas Cage" class="form-control" value="{{old('actor')}}">
                 <p class="help-block">Find by an actor</p>
             </div>
             <div class="form-group">
-                <label class=" control-label" for="year">Actor</label>
-                <input id="year" name="year" type="search" placeholder="E.g 2011" class="form-control" value="{{old('year')}}">
+                <label class=" control-label" for="year">Year</label>
+                <input id="year" name="year" type="search" placeholder="i.g 2011" class="form-control" value="{{old('year')}}">
                 <p class="help-block">Find by an year</p>
             </div>
             <div class="form-group">
                 <label class=" control-label" for="director">Director</label>
-                <input id="director" name="director" type="search" placeholder="E.g Quentin Tarantino" class="form-control" value="{{old('director')}}">
+                <input id="director" name="director" type="search" placeholder="i.g Quentin Tarantino" class="form-control" value="{{old('director')}}">
                 <p class="help-block">Find by a director</p>
             </div>
         </div>
