89a09733671674184fb50d842a015f2b0c857677
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-05-07 20:22:52 -0300

    basics api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['as' =>'movies', 'prefix' => 'movies'], function(){
-    Route::get('/', 'MoviesController@index');
-    Route::post('/', 'MoviesController@index');
+
+Route::group(['middleware'=>'web'], function (){
+
+    Route::auth();
+
+    Route::group(['as' =>'movies', 'prefix' => 'movies'], function(){
+        Route::get('/', 'MoviesController@index');
+        Route::post('/', 'MoviesController@index');
+    });
+
+    Route::get('/home', 'HomeController@index');
+
 });
 
-Route::auth();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['prefix' => 'api/v1', 'middleware' => 'auth:api', 'throttle:5'], function () {
+    Route::post('/', 'PeopleController@index');
+    Route::post('/add/', 'PeopleController@create');
+    Route::post('/update/', 'PeopleController@update');
+    Route::post('/delete/', 'PeopleController@delete');
+});
