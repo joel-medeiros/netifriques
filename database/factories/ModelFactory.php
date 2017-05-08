573578a4629ddc8e784d1aff720322af42459e86
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-05-07 22:53:09 -0300

    api token basics Factory creating first person

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,20 @@
 |
 */
 
+
+$factory->define(\App\People::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+    ];
+});
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'api_token' => str_random(60),
     ];
 });
